13d139063906bc7fbe35a002191cd5bcbe9234e9
Dionysis Zindros <dionyziz@gmail.com>
2012-01-01 17:03:23 +0000

    Fixed servers solution in O( N^3 )

--- ex2/code/servers/servers.cpp
+++ ex2/code/servers/servers.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 
+const int INFINITY = 10000;
+
 using namespace std;
 
 inline int min( int a, int b ) {
@@ -7,8 +9,9 @@ inline int min( int a, int b ) {
 }
 
 int main() {
-    int N, K, i, j;
-    int B[ 10000 ], C[ 10000 ], J[ 10000 ], JS[ 10000 ];
+    int N, K, i, j, l, cost;
+    int B[ 10000 ], C[ 10000 ];
+    int J[ 10000 ], JS[ 10000 ], W[ 10000 ];
 
     scanf( "%i", &N );
     K = -1;
@@ -20,16 +23,40 @@ int main() {
     }
     if ( K == -1 ) {
         printf( "0\n" );
+        return 0;
     }
 
-    JS[ K ] = 0;
+    JS[ K ] = C[ K ];
+    J[ K ] = C[ K ];
+    W[ K ] = K;
     for ( i = K - 1; i >= 1; --i ) {
         JS[ i ] = J[ i + 1 ] + C[ i ];
-        for ( j = i; j <= K; ++j ) {
-            J[ i ] = min( J[ i ], JS[ j ] + B[ i ] * ( j - i ) );
+        J[ i ] = INFINITY;
+        for ( j = i; j <= W[ i + 1 ]; ++j ) {
+            cost = JS[ j ];
+            for ( l = i; l <= j; ++l ) {
+                cost += B[ l ] * ( j - l );
+                if ( cost >= J[ i ] ) {
+                    break;
+                }
+            }
+            if ( cost < J[ i ] ) {
+                J[ i ] = cost;
+                W[ i ] = j;
+            }
         }
     }
-    printf( "%i\n", J[ 0 ] );
+    printf( "Minimized cost = %i\n", J[ 1 ] );
+    printf( "Dynamic programming solution:\n" );
+    for ( i = 1; i <= K; ++i ) {
+        printf( "( %i, %i, %i ) ", J[ i ], JS[ i ], W[ i ] );
+    }
+    printf( "\nServer configuration:\n" );
+    for ( i = W[ 1 ] + 1; i <= K; i = W[ i ] + 1 ) {
+        printf( "%i ", i - 1 );
+    }
+    printf( "%i", K );
+    printf( "\n" );
 
     return 0;
 }
