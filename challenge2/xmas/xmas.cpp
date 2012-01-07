fd416c1034813567170bfefa92872da4bb641ab7
Dionysis Zindros <dionyziz@gmail.com>
2012-01-07 01:59:38 +0000

    fixed compile errors in dynamic programming solution for xmas

--- challenge2/xmas/xmas.cpp
+++ challenge2/xmas/xmas.cpp
@@ -1,10 +1,11 @@
 #include <cstdio>
 #include <map>
 #include <utility>
+#include <cmath>
 
 using namespace std;
 
-const int INFINITY = 9876543210;
+const int INF = 123456789;
 
 typedef pair< int, int > point;
 typedef pair< bool, point > state;
@@ -15,6 +16,53 @@ point A, B;
 
 inline int min( int a, int b ) {
     return a < b? a: b;
+}
+
+inline int norm( point P, point Q ) {
+    return abs( ( double )( P.first - Q.first ) ) + abs( ( double )( P.second - Q.second ) );
+}
+
+int dynamic() {
+    point P[ 2 ];
+    utility J[ 2 ];
+    bool parity = false;
+    int cost;
+    state key;
+
+    scanf( "%i %i", &P[ parity ].first, &P[ parity ].second );
+    J[ parity ][ make_pair( 0, A ) ] = norm( A, P[ parity ] );
+    J[ parity ][ make_pair( 1, B ) ] = norm( B, P[ parity ] );
+
+    for ( int i = 1; i < N; ++i ) {
+        parity = !parity;
+        scanf( "%i %i", &P[ parity ].first, &P[ parity ].second );
+        J[ parity ].clear();
+        for ( utility::iterator u = J[ !parity ].begin(); u != J[ !parity ].end(); ++u ) {
+            J[ parity ][ ( *u ).first ] = ( *u ).second + norm( P[ parity ], P[ !parity ] );
+            key = make_pair( !( *u ).first.first, P[ !parity ] );
+            if ( J[ parity ].find( key ) == J[ parity ].end() ) {
+                J[ parity ][ key ] = INF;
+            }
+            J[ parity ][ key ] = min(
+                J[ parity ][ key ],
+                ( *u ).second + norm( ( *u ).first.second, P[ !parity ] )
+            );
+        }
+    }
+    cost = INF;
+    for ( utility::iterator u = J[ parity ].begin(); u != J[ parity ].end(); ++u ) {
+        cost = min( cost, u->second );
+    }
+    return cost;
+}
+
+int kMeans() {
+    point P[ N ];
+
+    for ( int i = 0; i < N; ++i ) {
+        scanf( "%i %i", &P[ i ].first, &P[ i ].second );
+    }
+    // TODO
 }
 
 int main() {
@@ -30,50 +78,4 @@ int main() {
     }
 
     return 0;
-}
-
-inline int operator - ( point P, point Q ) {
-    return abs( P.x - Q.x ) + abs( P.y - Q.y );
-}
-
-int dynamic() {
-    point P[ 2 ];
-    utility J[ 2 ];
-    bool parity = false;
-    int cost;
-
-    scanf( "%i %i", &P[ parity ].first, &P[ parity ].second );
-    J[ parity ][ make_pair( 0, A ) ] = A - P;
-    J[ parity ][ make_pair( 1, B ) ] = B - P;
-
-    for ( int i = 1; i < N; ++i ) {
-        parity = !parity;
-        scanf( "%i %i", &P.first, &P.second );
-        J[ parity ].clear();
-        for ( utility::iterator u = J[ !parity ].begin(); u != J[ !parity ].end(); ++u ) {
-            J[ parity ][ u->first ] = u->second + ( P[ parity ] - P[ !parity ] );
-            key = make_pair( !u->first->first, P[ !parity ] );
-            if ( J[ parity ].find( key ) == map.end() ) {
-                J[ parity ][ key ] = INFINITY;
-            }
-            J[ parity ][ key ] = min(
-                J[ parity ][ key ],
-                u->second + ( u->first->second - P[ !parity ] )
-            );
-        }
-    }
-    cost = INFINITY;
-    for ( utility::iterator u = J[ parity ].begin(); u != J[ parity ].end(); ++u ) {
-        cost = min( cost, u->second );
-    }
-    return cost;
-}
-
-int kMeans() {
-    point P[ N ];
-
-    for ( int i = 0; i < N; ++i ) {
-        scanf( "%i %i", &P[ i ].first, &P[ i ].second );
-    }
-    // TODO
 }
