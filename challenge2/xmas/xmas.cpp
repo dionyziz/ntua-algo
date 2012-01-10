743da69b7cfbfc64dc4926002e4cb4cc08d58906
Dionysis Zindros <dionyziz@gmail.com>
2012-01-10 22:26:54 +0000

    Optimizations on Christmas; moving from C++ data structures to plain C arrays and using some fast memset

--- challenge2/xmas/xmas.cpp
+++ challenge2/xmas/xmas.cpp
@@ -1,140 +1,167 @@
 #include <cstdio>
 #include <cstdlib>
-#include <map>
 #include <vector>
-#include <utility>
 #include <cmath>
+#include <cstring>
 
 using namespace std;
+
+#define MIN(a,b)  ((a) < (b) ? (a) : (b))
 
 const int INF = 987654321;
 const float EPSILON = 0.00001;
 
-typedef pair< int, int > point;
-typedef pair< float, float > fpoint;
-typedef pair< bool, point > state;
-typedef map< state, int > utility;
-
 int N, R, C;
-point A, B;
+int A[ 2 ], B[ 2 ];
+int J[ 2 ][ 2 ][ 10 ][ 10 ];
 
-inline int min( int a, int b ) {
-    return a < b? a: b;
+inline int norm( int P[ 2 ], int Q[ 2 ] ) {
+    return abs( P[ 0 ] - Q[ 0 ] ) + abs( P[ 1 ] - Q[ 1 ] );
 }
 
-inline int norm( point P, point Q ) {
-    return abs( ( double )( P.first - Q.first ) ) + abs( ( double )( P.second - Q.second ) );
+inline float fnorm( int P[ 2 ], float Q[ 2 ] ) {
+    return fabs( ( float )P[ 0 ] - Q[ 0 ] ) + fabs( ( float )P[ 1 ] - Q[ 1 ] );
 }
 
-inline float fnorm( point P, fpoint Q ) {
-    return fabs( ( float )P.first - Q.first ) + fabs( ( float )P.second - Q.second );
+int printJ( int parity ) {
+    for ( int agent = 0; agent < 2; ++agent ) {
+        printf( "Agent %i:\n", agent );
+        for ( int x = 0; x < C; ++x ) {
+            for ( int y = 0; y < R; ++y ) {
+                if ( J[ parity ][ agent ][ x ][ y ] > 100 ) {
+                    printf( "INF " );
+                }
+                else {
+                    printf( "%i   ", J[ parity ][ agent ][ x ][ y ] );
+                }
+            }
+            printf( "\n" );
+        }
+    }
 }
 
 int dynamic() {
-    point P[ 2 ];
-    utility J[ 2 ];
+    int P[ 2 ][ 2 ];
     bool parity = false;
     int cost;
-    state key;
 
-    scanf( "%i %i", &P[ parity ].first, &P[ parity ].second );
-    J[ parity ][ make_pair( 0, B ) ] = norm( A, P[ parity ] );
-    J[ parity ][ make_pair( 1, A ) ] = norm( B, P[ parity ] );
+    scanf( "%i %i", &P[ parity ][ 0 ], &P[ parity ][ 1 ] );
+    --P[ parity ][ 0 ]; --P[ parity ][ 1 ];
+
+    memset( J[ parity ], 120, 2 * 10 * 10 * sizeof( int ) );
+    // printf( "J after iteration 1: " );
+    // printJ( parity );
+
+    // printf( "A = ( %i, %i ). B = ( %i, %i ).\n", A[ 0 ], A[ 1 ], B[ 0 ], B[ 1 ] );
+
+    J[ parity ][ 0 ][ B[ 0 ] ][ B[ 1 ] ] = norm( A, P[ parity ] );
+    J[ parity ][ 1 ][ A[ 0 ] ][ A[ 1 ] ] = norm( B, P[ parity ] );
+
+    // printf( "J after iteration 1: " );
+    // printJ( parity );
 
     for ( int i = 1; i < N; ++i ) {
-      //  printf( "Dumping utility function before induction step %i over point [%i, %i]:\n", i, P[ parity ].first, P[ parity ].second );
-      //  for ( utility::iterator u = J[ parity ].begin(); u != J[ parity ].end(); ++u ) {
-      //      printf( "( %s, [ %i, %i ] ) = %i\n", u->first.first? "B": "A", u->first.second.first, u->first.second.second, u->second );
-      //  }
-      //  printf( "\n" );
-
+        // printf( "J after iteration %i: ", i + 1 );
         parity = !parity;
-        scanf( "%i %i", &P[ parity ].first, &P[ parity ].second );
-        J[ parity ].clear();
+        scanf( "%i %i", &P[ parity ][ 0 ], &P[ parity ][ 1 ] );
+        --P[ parity ][ 0 ]; --P[ parity ][ 1 ];
+        memset( J[ parity ], 120, 2 * 10 * 10 * sizeof( int ) );
         // look at the previous utility function and update accordingly
-        for ( utility::iterator u = J[ !parity ].begin(); u != J[ !parity ].end(); ++u ) {
-            if ( J[ parity ].find( u->first ) == J[ parity ].end() ) {
-                J[ parity ][ u->first ] = INF;
+        for ( int agent = 0; agent < 2; ++agent ) {
+            for ( int x = 0; x < C; ++x ) {
+                for ( int y = 0; y < R; ++y ) {
+                    int prevJ = J[ !parity ][ agent ][ x ][ y ];
+                    int thisX = P[ parity ][ 0 ];
+                    int thisY = P[ parity ][ 1 ];
+                    int prevX = P[ !parity ][ 0 ];
+                    int prevY = P[ !parity ][ 1 ];
+
+                    // same key: two consequent forces of the same agent
+                    J[ parity ][ agent ][ x ][ y ] = MIN(
+                        J[ parity ][ agent ][ x ][ y ],
+                        prevJ
+                        // norm
+                        + abs( thisX - prevX )
+                        + abs( thisY - prevY )
+                    );
+
+                    // different key: the two agents have swapped during this and the previous induction step
+                    J[ parity ][ !agent ][ prevX ][ prevY ] = MIN(
+                        J[ parity ][ !agent ][ prevX ][ prevY ],
+                        prevJ
+                        // norm
+                        + abs( x - thisX )
+                        + abs( y - thisY )
+                    );
+                }
             }
-            // same key: two consequent forces of the same agent
-            J[ parity ][ u->first ] = min(
-                J[ parity ][ u->first ],
-                u->second + norm( P[ parity ], P[ !parity ] )
-            );
-            // different key: the two agents have swapped during this and the previous induction step
-            key = make_pair( !u->first.first, P[ !parity ] );
-            if ( J[ parity ].find( key ) == J[ parity ].end() ) {
-                J[ parity ][ key ] = INF;
-            }
-            J[ parity ][ key ] = min(
-                J[ parity ][ key ],
-                u->second + norm( u->first.second, P[ parity ] )
-            );
         }
     }
-  //  printf( "Dumping utility function after final induction step over point [%i, %i]:\n", P[ parity ].first, P[ parity ].second );
-  //  for ( utility::iterator u = J[ parity ].begin(); u != J[ parity ].end(); ++u ) {
-  //      printf( "( %s, [ %i, %i ] ) = %i\n", u->first.first? "B": "A", u->first.second.first, u->first.second.second, u->second );
-  //  }
-  //  printf( "\n" );
 
     cost = INF;
-    for ( utility::iterator u = J[ parity ].begin(); u != J[ parity ].end(); ++u ) {
-        cost = min( cost, u->second );
+    for ( int agent = 0; agent < 2; ++agent ) {
+        for ( int x = 0; x < C; ++x ) {
+            for ( int y = 0; y < R; ++y ) {
+                cost = MIN( cost, J[ parity ][ agent ][ x ][ y ] );
+            }
+        }
     }
     return cost;
 }
 
 int kMeans() {
-    point P[ N ];
+    int P[ N ][ 2 ];
     vector< bool > cluster( N );
     int assignCounts[ 2 ];
-    fpoint means[ 2 ];
-    point T;
+    float means[ 2 ][ 2 ];
+    int T[ 2 ];
     int cost;
 
     for ( int i = 0; i < N; ++i ) {
-        scanf( "%i %i", &P[ i ].first, &P[ i ].second );
+        scanf( "%i %i", &P[ i ][ 0 ], &P[ i ][ 1 ] );
+        --P[ i ][ 0 ]; --P[ i ][ 1 ];
         // randomly partition the dataset
         cluster[ i ] = rand() % 2 == 1;
     }
 
     for ( int iteration = 0; iteration < 10; ++iteration ) {
         // update step: calculate new means
-        means[ 0 ] = make_pair( 0.0, 0.0 );
-        means[ 1 ] = make_pair( 0.0, 0.0 );
+        means[ 0 ][ 0 ] = 0.0;
+        means[ 0 ][ 1 ] = 0.0;
+        means[ 1 ][ 0 ] = 0.0;
+        means[ 1 ][ 1 ] = 0.0;
         assignCounts[ 0 ] = assignCounts[ 1 ] = 0;
         for ( int i = 0; i < N; ++i ) {
             printf( "Means in %i-th iteration after %i-th point: A = ( %2.2f, %2.2f ), B = ( %2.2f, %2.2f ).\n",
-                    iteration, i, means[ 0 ].first, means[ 0 ].second, means[ 1 ].first, means[ 1 ].second );
-            means[ cluster[ i ] ].first += ( float )P[ i ].first;
-            means[ cluster[ i ] ].second += ( float )P[ i ].second;
+                    iteration, i, means[ 0 ][ 0 ], means[ 0 ][ 1 ], means[ 1 ][ 0 ], means[ 1 ][ 1 ] );
+            means[ cluster[ i ] ][ 0 ] += ( float )P[ i ][ 0 ];
+            means[ cluster[ i ] ][ 1 ] += ( float )P[ i ][ 1 ];
             ++assignCounts[ cluster[ i ] ];
         }
         if ( assignCounts[ 0 ] == 0 ) {
             // remove cluster 0
-            means[ 0 ] = make_pair( -1.0, -1.0 );
+            means[ 0 ][ 0 ] = -1.0;
         }
         else {
-            means[ 0 ].first /= ( float )assignCounts[ 0 ];
-            means[ 0 ].second /= ( float )assignCounts[ 0 ];
+            means[ 0 ][ 0 ] /= ( float )assignCounts[ 0 ];
+            means[ 0 ][ 1 ] /= ( float )assignCounts[ 0 ];
         }
         if ( assignCounts[ 0 ] == 1 ) {
             // remove cluster 1
-            means[ 1 ] = make_pair( -1.0, -1.0 );
+            means[ 1 ][ 0 ] = -1.0;
         }
         else {
-            means[ 1 ].first /= ( float )assignCounts[ 1 ];
-            means[ 1 ].second /= ( float )assignCounts[ 1 ];
+            means[ 1 ][ 0 ] /= ( float )assignCounts[ 1 ];
+            means[ 1 ][ 1 ] /= ( float )assignCounts[ 1 ];
         }
         printf( "Means in %i-th iteration: A = ( %2.2f, %2.2f ), B = ( %2.2f, %2.2f ).\n",
-                iteration, means[ 0 ].first, means[ 0 ].second, means[ 1 ].first, means[ 1 ].second );
+                iteration, means[ 0 ][ 0 ], means[ 0 ][ 1 ], means[ 1 ][ 0 ], means[ 1 ][ 1 ] );
 
         // assignment step: assign each data point to the cluster whose mean it's closest to
         for ( int i = 0; i < N; ++i ) {
             if ( ( fnorm( P[ i ], means[ 0 ] ) > fnorm( P[ i ], means[ 1 ] )
-                   || means[ 0 ].first + 1 < EPSILON )
-                 && means[ 1 ].first + 1 > EPSILON ) {
+                   || means[ 0 ][ 0 ] + 1 < EPSILON )
+                 && means[ 1 ][ 0 ] + 1 > EPSILON ) {
                 cluster[ i ] = 1;
             }
             else {
@@ -142,14 +169,17 @@ int kMeans() {
             }
         }
     }
-    if ( norm( A, means[ 0 ] ) + norm( B, means[ 1 ] )
-       > norm( A, means[ 1 ] ) + norm( B, means[ 0 ] ) ) {
+    if ( fnorm( A, means[ 0 ] ) + fnorm( B, means[ 1 ] )
+       > fnorm( A, means[ 1 ] ) + fnorm( B, means[ 0 ] ) ) {
         // assign initial points optimally to cluster centers
-        T = A;
-        A = B;
-        B = T;
+        T[ 0 ] = A[ 0 ];
+        T[ 1 ] = A[ 1 ];
+        A[ 0 ] = B[ 0 ];
+        A[ 1 ] = B[ 1 ];
+        B[ 0 ] = T[ 0 ];
+        B[ 1 ] = T[ 1 ];
     }
-    printf( "Cluster centers at A = ( %i, %i ), B = ( %i, %i ).\n", means[ 0 ].first, means[ 0 ].second, means[ 1 ].first, means[ 1 ].second );
+    printf( "Cluster centers at A = ( %i, %i ), B = ( %i, %i ).\n", means[ 0 ][ 0 ], means[ 0 ][ 1 ], means[ 1 ][ 0 ], means[ 1 ][ 1 ] );
     cost = 0;
     for ( int i = 0; i < N; ++i ) {
         // calculate the cost of travel assigning each point to the agent
@@ -157,12 +187,14 @@ int kMeans() {
         if ( cluster[ i ] ) {
             // printf( "%i belongs to B.\n", i + 1 );
             cost += norm( B, P[ i ] );
-            B = P[ i ];
+            B[ 0 ] = P[ i ][ 0 ];
+            B[ 1 ] = P[ i ][ 1 ];
         }
         else {
             // printf( "%i belongs to A.\n", i + 1 );
             cost += norm( A, P[ i ] );
-            A = P[ i ];
+            A[ 0 ] = P[ i ][ 0 ];
+            A[ 1 ] = P[ i ][ 1 ];
         }
     }
     return cost;
@@ -173,11 +205,13 @@ int main() {
     //         1 <= R, C <= 10
     // rarely: 1 <= R, C <= 1000
     
-    scanf( "%i %i %i", &N, &R, &C );
-    scanf( "%i %i", &A.first, &A.second );
-    scanf( "%i %i", &B.first, &B.second );
+    scanf( "%i %i %i", &N, &C, &R );
+    scanf( "%i %i", &A[ 0 ], &A[ 1 ] );
+    scanf( "%i %i", &B[ 0 ], &B[ 1 ] );
+    --A[ 0 ]; --A[ 1 ];
+    --B[ 0 ]; --B[ 1 ];
 
-    if ( R * C > 110 ) {
+    if ( R > 10 || C > 10 ) {
         // dynamic programming is too slow, so use a k-means heuristic in O( N )
         // printf( "Using k-means algorithm.\n" );
         printf( "%i\n", kMeans() );
