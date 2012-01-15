83d72264416f4091b34ba0afb41140c972eb48e7
Dionysis Zindros <dionyziz@gmail.com>
2012-01-15 14:05:13 +0000

    Fix off-by-one errors in teleport solution

--- challenge2/teleport/teleport.cpp
+++ challenge2/teleport/teleport.cpp
@@ -18,22 +18,42 @@ arrow A[ 500010 ];
 int J[ 500010 ];
 
 int cmp( const void* a, const void* b ) {
-    return ( ( arrow* )a )->s - ( ( arrow* )b )->s;
+    arrow* one = ( arrow* )a;
+    arrow* two = ( arrow* )b;
+
+    int s = one->s - two->s;
+    if ( s != 0 ) {
+        return s;
+    }
+    return one->f - two->f;
+}
+
+int printJ( int i ) {
+    printf( "Cost function after %i-th induction step:\n", i );
+
+    for ( int j = 1; j <= i; ++j ) {
+        printf( "J[ %i ] = %i\n", j, J[ j ] );
+    }
 }
 
 int main() {
     int max = 0;
 
     scanf( "%i", &N );
-    for ( int i = 0; i < N; ++i ) {
+    J[ 0 ] = -1;
+    for ( int i = 1; i <= N; ++i ) {
         scanf( "%i %i", &A[ i ].s, &A[ i ].f );
-        J[ i + 1 ] = INF;
+        J[ i ] = INF;
     }
-    qsort( A, N, sizeof( arrow ), cmp );
+    qsort( A + 1, N, sizeof( arrow ), cmp );
+  //  for ( int i = 0; i < N; ++i ) {
+  //      printf( "( %i, %i )\n", A[ i ].s, A[ i ].f );
+  //  }
+  //  printf( "\n" );
     // LIS
-    for ( int i = 0; i < N; ++i ) {
+    for ( int i = 1; i <= N; ++i ) {
         int x = A[ i ].f;
-        int mid, low = 1, high = i;
+        int mid, low = 0, high = i;
         int j;
 
         // binary search
@@ -50,9 +70,12 @@ int main() {
         if ( low == high - 1 ) {
             j = low;
         }
+        // printf( "Updating item %i\n", j );
 
         max = MAX( j + 1, max );
         J[ j + 1 ] = MIN( x, J[ j + 1 ] );
+
+        // printJ( i );
     }
     printf( "%i\n", max );
 
