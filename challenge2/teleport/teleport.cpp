18e93127785270afb8a2a4fdd7e8f8e1ba3911d9
Dionysis Zindros <dionyziz@gmail.com>
2012-01-15 12:34:35 +0000

    Finished the teleport problem

--- challenge2/teleport/teleport.cpp
+++ challenge2/teleport/teleport.cpp
@@ -3,44 +3,54 @@
 
 using namespace std;
 
-#define MIN(a,b)  ((a) < (b) ? (a) : (b))
-#define MAX(a,b)  ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+const int INF = 987654321;
 
 struct arrow {
     int s;
     int f;
 };
 
+int N;
+arrow A[ 500010 ];
+int J[ 500010 ];
+
 int cmp( const void* a, const void* b ) {
     return ( ( arrow* )a )->s - ( ( arrow* )b )->s;
 }
 
 int main() {
-    int N;
-    arrow A[ 500010 ];
     int max = 0;
-    int J[ 500010 ];
 
     scanf( "%i", &N );
     for ( int i = 0; i < N; ++i ) {
         scanf( "%i %i", &A[ i ].s, &A[ i ].f );
+        J[ i + 1 ] = INF;
     }
     qsort( A, N, sizeof( arrow ), cmp );
+    // LIS
     for ( int i = 0; i < N; ++i ) {
         int x = A[ i ].f;
-        int low = 0, high = i;
+        int mid, low = 1, high = i;
         int j;
 
-        while ( low != high ) {
+        // binary search
+        while ( low < high - 1 ) {
             mid = ( low + high ) / 2;
             if ( J[ mid ] > x ) {
-                high = mid - 1;
+                high = mid;
             }
-            else if ( J[ mid ] <= x ) {
+            else {
                 low = mid;
             }
         }
-        j = low;
+        j = 0;
+        if ( low == high - 1 ) {
+            j = low;
+        }
+
         max = MAX( j + 1, max );
         J[ j + 1 ] = MIN( x, J[ j + 1 ] );
     }
